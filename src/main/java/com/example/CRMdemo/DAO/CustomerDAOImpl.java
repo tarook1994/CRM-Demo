2adbd6d466edf05d10c7dd35123c8fd002d99881
Ahmed Tarek <ahmed-tarek94@hotmail.com>
2019-05-01 18:39:47 +0200

    Fixed server crash bug

--- src/main/java/com/example/CRMdemo/DAO/CustomerDAOImpl.java
+++ src/main/java/com/example/CRMdemo/DAO/CustomerDAOImpl.java
@@ -17,37 +17,29 @@ public class CustomerDAOImpl implements CustomerDAO {
     @Autowired
     SessionFactory sessionFactory;
 
+
+
     @Override
-    @Transactional
     public List<Customer> getCustomers() {
 
 
         Session session = sessionFactory.getCurrentSession();
-        try {
             session.beginTransaction();
           //  Customer c = (Customer) session.get(Customer.class, 1);
             List<Customer> customerList = session.createQuery("from Customer").list();
             session.getTransaction().commit();
             return customerList;
-        } finally {
 
-            sessionFactory.close();
-        }
     }
 
     @Override
     public Customer getCustomerById(int id) {
         Session session = sessionFactory.getCurrentSession();
-        try {
             session.beginTransaction();
             //  Customer c = (Customer) session.get(Customer.class, 1);
             Customer customer = (Customer) session.get(Customer.class ,id);
             session.getTransaction().commit();
             return customer;
-        } finally {
 
-            sessionFactory.close();
-
-        }
     }
 }
