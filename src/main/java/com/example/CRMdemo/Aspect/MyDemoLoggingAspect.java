06315d213fab90bad01d8d4b1549916ad59d51ea
Ahmed Tarek <ahmed-tarek94@hotmail.com>
2019-05-18 13:50:39 +0200

    Adding Around

--- src/main/java/com/example/CRMdemo/Aspect/MyDemoLoggingAspect.java
+++ src/main/java/com/example/CRMdemo/Aspect/MyDemoLoggingAspect.java
@@ -1,9 +1,8 @@
 package com.example.CRMdemo.Aspect;
 
 
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -21,6 +20,26 @@ public class MyDemoLoggingAspect {
     @Before("anyDAOMethod()")
     public void logBeforeDAO(){
         System.out.println("Logging");
+    }
+
+
+    @After("anyDAOMethod()")
+    public void logAfterDAO(){
+        System.out.println("After DAO");
+    }
+
+    @Around("anyDAOMethod()")
+    public Object timeCalculation(ProceedingJoinPoint proceedingJoinPoint){
+        long begin = System.currentTimeMillis();
+        Object result = null;
+        try {
+             result = proceedingJoinPoint.proceed();
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+        long end = System.currentTimeMillis();
+        System.out.println(end-begin + " Milliseconds" + result);
+        return result;
     }
 
 
